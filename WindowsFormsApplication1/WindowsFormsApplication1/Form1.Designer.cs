f795cee900f6ecaedffa32809e0399703b547323
Chethiya2001 <chethiyabandaraherath@gmail.com>
2022-08-04 21:59:35 +0530

    c # application

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxEmpID = new System.Windows.Forms.TextBox();
+            this.tbxSalary = new System.Windows.Forms.TextBox();
+            this.tbxEmpName = new System.Windows.Forms.TextBox();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 208);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gender";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 143);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 78);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Employee Name";
+            // 
+            // tbxEmpID
+            // 
+            this.tbxEmpID.Location = new System.Drawing.Point(257, 15);
+            this.tbxEmpID.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxEmpID.Name = "tbxEmpID";
+            this.tbxEmpID.Size = new System.Drawing.Size(364, 22);
+            this.tbxEmpID.TabIndex = 4;
+            // 
+            // tbxSalary
+            // 
+            this.tbxSalary.Location = new System.Drawing.Point(257, 148);
+            this.tbxSalary.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxSalary.Name = "tbxSalary";
+            this.tbxSalary.Size = new System.Drawing.Size(364, 22);
+            this.tbxSalary.TabIndex = 6;
+            // 
+            // tbxEmpName
+            // 
+            this.tbxEmpName.Location = new System.Drawing.Point(257, 84);
+            this.tbxEmpName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxEmpName.Name = "tbxEmpName";
+            this.tbxEmpName.Size = new System.Drawing.Size(364, 22);
+            this.tbxEmpName.TabIndex = 7;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Location = new System.Drawing.Point(257, 215);
+            this.rbtnMale.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(59, 21);
+            this.rbtnMale.TabIndex = 8;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Location = new System.Drawing.Point(257, 244);
+            this.rbtnFemale.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(75, 21);
+            this.rbtnFemale.TabIndex = 9;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnSearch.Location = new System.Drawing.Point(80, 304);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(129, 46);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.Color.Teal;
+            this.btnInsert.Location = new System.Drawing.Point(217, 304);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(129, 46);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(354, 304);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.Color.Crimson;
+            this.btndelete.Location = new System.Drawing.Point(491, 304);
+            this.btndelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(129, 46);
+            this.btndelete.TabIndex = 13;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Magenta;
+            this.btnExit.Location = new System.Drawing.Point(277, 358);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(129, 46);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 404);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.rbtnFemale);
+            this.Controls.Add(this.rbtnMale);
+            this.Controls.Add(this.tbxEmpName);
+            this.Controls.Add(this.tbxSalary);
+            this.Controls.Add(this.tbxEmpID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxEmpID;
+        private System.Windows.Forms.TextBox tbxSalary;
+        private System.Windows.Forms.TextBox tbxEmpName;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsFormsApplication1
+
+
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-5FR5VA3;Initial Catalog=EMP;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void clear_controls()
+        {
+            tbxEmpID.Clear();
+            tbxEmpName.Clear();
+            tbxSalary.Clear();
+            rbtnMale.Checked = false;
+            rbtnFemale.Checked = false;
+            tbxEmpID.Focus();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string SqlSearch;
+            SqlSearch = "select * from emp_table where empno='"+tbxEmpID.Text+"' ";
+            SqlCommand cmd=new SqlCommand (SqlSearch,con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                tbxEmpName.Text = dr["name"].ToString();
+                tbxSalary.Text = dr["salary"].ToString();
+
+                String Gender;
+                Gender = dr["gender"].ToString();
+                if(Gender=="M")
+                {
+                    rbtnMale.Checked = true;
+                }
+                else
+                {
+                    rbtnFemale.Checked = true;
+                }
+            con.Close();
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Not Found");
+                clear_controls();
+            }
+            
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string gender;
+            if (rbtnMale.Checked == true)
+                gender = "M";
+            else
+                gender = "F";
+
+            string sqlinsert;
+            sqlinsert = "insert into emp_table (empno,name,salary,gender) values('"+tbxEmpID.Text+"','"+tbxEmpName.Text+"','"+tbxSalary.Text+"','"+gender+"')";
+            SqlCommand cmd = new SqlCommand(sqlinsert, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Inserted");
+            clear_controls();
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string gender;
+            if (rbtnMale.Checked == true)
+                gender = "M";
+            else
+                gender = "F";
+
+            string sqlupdate;
+            sqlupdate = "update emp_table set name='"+tbxEmpName.Text+"',salary='"+tbxSalary.Text+"', gender='"+gender+"' where empno='"+gender+"' ";
+            SqlCommand cmd = new SqlCommand(sqlupdate, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show(" Succesfully Update... ");
+            clear_controls();
+            con.Close();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Bye");
+            Application.Exit();
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            DialogResult ans;
+            ans = MessageBox.Show("Are you to Delete!", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (ans==DialogResult.Yes)
+            {
+                string sqldelete;
+                sqldelete = "delete from emp_table where empno ='" + tbxEmpID + "'";
+                SqlCommand cmd = new SqlCommand(sqldelete, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("deleted");
+                clear_controls();
+                con.Close();
+            }
+        }
+    }
+}
